6509d2c69482a0fb5303888391134be817825635
StasLakh <doctorbyron05@gmail.com>
2016-10-19 07:12:04 -0700

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @users = User.order(params[:sort])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+
+end
